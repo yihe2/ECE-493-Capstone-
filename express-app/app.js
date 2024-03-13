3092477a1d0c854815b37341a1ec08d5668f0f8a
Arjun <arjun.mehta1001@gmail.com>
2024-03-13 13:04:22 -0600

    mongo setup

---
+++ express-app/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
