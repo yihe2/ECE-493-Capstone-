4c1e32d41a452050ad6e27684628f5272dc6d346
Arjun <arjun.mehta1001@gmail.com>
2024-03-13 09:50:20 -0600

    create account modifications made

--- front-end/health-yeah/src/App.js
+++ front-end/health-yeah/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Login from './pages/Login';
+import CreateAccount from './pages/CreateAccount';
 
 import React from 'react';
 
@@ -21,6 +22,7 @@ const App = () => {
         {/* does not work */}
         <Route path="/" element={<div className='items-center p-32 bg-black'>penis balls butt</div>}/>
         <Route path="/login" element={<Login></Login>} />
+        <Route path="/create-account" element={<CreateAccount/>}/>
       </Routes>
     </BrowserRouter>
   );
