09a7c0959533cefc5b9342361f2ae7f3b757e5cb
Tobrown3 <saifhusnain@gmail.com>
2024-03-18 16:19:05 -0600

    Added express endpoint

--- express-app/app.js
+++ express-app/app.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+app.use(express.json());
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
   res.send('Hello, Express!');
+});
+
+app.post('/user/info', (requ, res) => {
+  const info = req.body;
+  console.log(info);
+  res.json({status: 'success'});
 });
 
 // Start the server
