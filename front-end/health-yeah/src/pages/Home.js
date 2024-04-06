6b6a16a06fe566f9e07ed00be99f6cb082b2d01a
Arjun <arjun.mehta1001@gmail.com>
2024-04-05 20:26:37 -0600

    unpolished UI complete:

--- front-end/health-yeah/src/pages/Home.js
+++ front-end/health-yeah/src/pages/Home.js
@@ -13,6 +13,7 @@ const Home = () => {
   const [finPlan, setFinPlan] = useState('');
   const [targetRisk, setTargetRisk] = useState('0.3');
   const [error, setError] = useState('');
+  const [finError, setFinError] = useState('');
 
 
   const handleChange = (event) => {
@@ -44,13 +45,38 @@ const Home = () => {
         {
           withCredentials: true,
         });
-        console.log(data)
+
 
         if(data.status === 200) {
           console.log("in the 200")
-          setShowScore(true)
+          setShowFinPlan(true)
           // setShowFinPlan(`${data.data.score.toFixed(5) * 100}%`)
+          
           console.log(data.data)
+          console.log(data.data.monthly_installment)
+          if (typeof  data.data.monthly_installment === "number") {
+            console.log("This works")
+            switch(data.data.monthly_installment) {
+              // -1: 
+              // -2: You can already cover the cost on your own, you don't need to save extra money.
+              // -3: 
+              case -1:
+                setFinPlan(`Congratulations! You won't reach that risk level until at least age 80!`)
+                break;
+              case -2:
+                setFinPlan(`You can already cover the cost on your own, you don't need to save extra money.\n\n
+                          Our suggestion: ${data.data.suggested_actions}`)
+                break;
+              case -3:
+                setFinPlan(`You inputted a lower risk level than your current one.`)
+                break;
+            }
+          }
+          else {
+            setFinPlan(`We predict you will reach the target risk level in ${data.data.years} years.\n\n
+            Based on this we recommend you save an extra ${data.data.monthly_installment} per month.\n\n
+            Our advice: ${data.data.suggested_actions}`)
+          }
         }
         else {
           setFinPlan(true)
@@ -165,21 +191,19 @@ const Home = () => {
             </button>
         </div>
         <div className="max-w-lg p-8 bg-white shadow-lg rounded-lg mt-4">
-            <h1 className="text-3xl font-bold mb-4">Financial Plan</h1>
+            <h1 className="text-3xl font-bold mb-4">Future Risk</h1>
             <p className="text-gray-700 mb-4">
-              Click the button below to calculate your Financial Plan!
+              Click the button below to calculate your Future Risks!
             </p>
             {showFinPlan ? (
               <>
-              <p>Based on your Finances, Health Score, and target risk our model has determined your risk level to be: </p>
-              <p className='text-gray-700 mb-4'>{finPlan}</p>
-              {/* TODO: More intense case handling for negative codes */}
+                <p>Based on your Finances, Health Score, and target risk our model has determined your risk level to be: </p>
+                <p className='text-gray-700 mb-4'>{finPlan}</p>
+                {/* TODO: More intense case handling for negative codes */}
               </>
             ) : (
               <></>
             )}
-
-            {/* INSERT FORM TO SUBMIT INFO */}
 
             <form onSubmit={handleSubmit}>
               <label>
