1fd97a5debb45f652673bea6e0361e03385457fb
Arjun <arjun.mehta1001@gmail.com>
2024-04-11 10:37:18 -0600

    bug fix

--- front-end/health-yeah/src/App.js
+++ front-end/health-yeah/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React, {useContext} from 'react';
 import Login from './pages/Login';
 import CreateAccount from './pages/CreateAccount';
-import SecretTest from "./pages/SecretTest"
 import HealthInfoForm from './pages/HealthInfoForm';
 import FinInfoForm from './pages/FinInfoForm';
 import ChangePassword from './pages/ChangePassword';
@@ -24,7 +23,6 @@ const App = () => {
         <Route path="/" element={<Home />}/>
         <Route path="/login" element={<Login />} />
         <Route path="/create-account" element={<CreateAccount/>}/>
-        <Route path="/secret" element={<SecretTest />} />
         <Route path="/healthinfo" element= {<HealthInfoForm />}/>
         <Route path="/fininfo" element= {<FinInfoForm />}/>
         <Route path="/change-password" element= {<ChangePassword />}/>
